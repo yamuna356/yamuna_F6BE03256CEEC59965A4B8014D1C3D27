f3728ca8f4de2c469c140488309e1a37c1827f3e
yamuna356 <147043545+yamuna356@users.noreply.github.com>
2023-10-11 20:11:56 +0530

    challenge 2.1.py

--- challenge 2.1.py
+++ challenge 2.1.py
@@ -32,7 +32,7 @@ class BankAccount:
         self.__account_balance))
 # Create an instance of the BankAccount class
 account = BankAccount(account_number="123456789",
-                      account_holder_name="Lokesh",
+                      account_holder_name="yamuna",
                       initial_balance=5000.0)
 # Test deposit and withdrawal functionality
 account.display_balance()
